fb58ef48ac0c80aeb8b83c928c2b0026dacd906c
Wallace Junior <soareswallace@icloud.com>
2020-08-20 21:01:32 -0300

    Creating user repository

---
+++ src/app/Repository/UserRepository.php
@@ -0,0 +1,26 @@
+<?php
+
+
+namespace App\Repository;
+
+
+use App\User;
+use Illuminate\Support\Facades\DB;
+
+class UserRepository
+{
+    public function performDebitForUser(User $user, float $value)
+    {
+        DB::table('users')->where('id', $user->id)->update(['balance' => $user->balance - $value]);
+    }
+
+    public function perforCreditForUser(User $user, float $value)
+    {
+        DB::table('users')->where('id', $user->id)->update(['balance' => $user->balance + $value]);
+    }
+
+    public function findUserById($id)
+    {
+        return User::find($id);
+    }
+}
